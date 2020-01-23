db4eff27326f38503a503d9ff45f0609de3e87c7
rihofujino <r-fujino@tribalmedia.co.jp>
2020-01-23 18:33:53 +0900

    edit client.js

--- examples/rich-text/client.js
+++ examples/rich-text/client.js
@@ -19,14 +19,22 @@ window.connect = function() {
 
 // Create local Doc instance mapped to 'examples' collection document with id 'richtext'
 var doc = connection.get('examples', 'richtext');
+//console.log(doc);
+connection.fetchSnapshot('examples', 'richtext', 44, function (err, results) {
+    //console.log(results);
+});
+connection.fetchSnapshotByTimestamp('examples', 'richtext', 1579771022074, function (err, snapshot) {
+    // console.log(err);
+    // console.log(snapshot);
+});
 doc.subscribe(function(err) {
   if (err) throw err;
   var quill = new Quill('#editor', {theme: 'snow'});
   quill.setContents(doc.data);
     quill.on('text-change', function (delta, oldDelta, source) {
-      console.log(delta);
-      console.log(oldDelta);
-      console.log(source);
+    //   console.log(delta);
+    //   console.log(oldDelta);
+    //   console.log(source);
     if (source !== 'user') return;
     doc.submitOp(delta, {source: quill});
   });
